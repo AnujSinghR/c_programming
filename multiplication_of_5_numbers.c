5bdad500c1dc969d2fe78dc3037055709774f2d7
AnujSinghR <anujsingh0r@gmail.com>
2022-12-05 20:29:31 +0530

    hlo

--- multiplication_of_5_numbers.c
+++ multiplication_of_5_numbers.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-float avg(int,int,int);
+int m_of_5(int,int,int,int,int);
 main(){
-    int a,b,c;
-    float d;
-    printf("enter a,b and c");
-    scanf("%d%d%d",&a,&b,&c);
-    d=avg(a,b,c);
-    printf("%d",d);
+    int a,b,c,d,e,f;
+    printf("enter 5 numbers \n");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    f=m_of_5(a,b,c,d,e);
+    printf("%d",f);
 }
-float avg(int a,int b,int c){
-    float d;
-    d=(float)(a+b+c)/3;
-    return d;
+m_of_5(int a,int b,int c,int d,int e){
+    int f;
+    f=a*b*c*d*e;
+    return f;
 }
