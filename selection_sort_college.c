4b7e589aa1648b7b1f86776e153fa210d626e040
AnujSinghR <anujsingh0r@gmail.com>
2023-06-22 09:42:07 +0530

    sort

--- selection_sort_college.c
+++ selection_sort_college.c
@@ -1,34 +1,64 @@
 #include <stdio.h>
-int main()
-{
-int arr[5], length = 5, i, j, temp,n,min;
-printf("Enter the number of elements : ");
-scanf("%d",&n);
-printf("Enter %d numbers : ",n);
-for (i = 0; i < n; i++)
-{
-scanf("%d",&arr[i]);
-}
-for (i = 0; i < n-1; i++)
-{
-min = i;
-for (j = i+1; j < n; j++)
-if (arr[j] < arr[min])
-min = j;
 
-temp=arr[min];
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-arr[min]=arr[i];
-arr[i]=temp;
-for (i = 0; i < n; i++)
-{
-printf(" %d ",arr[i]);
+void selectionSort(int arr[], int size) {
+    int i, j, minIndex;
+
+    for (i = 0; i < size - 1; i++) {
+        minIndex = i;
+
+        for (j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            swap(&arr[i], &arr[minIndex]);
+        }
+         printf("arr in progress \n");
+    for(int ms=0;ms<=size-1;ms++){
+       
+        printf("%d",arr[ms]);
+    }
+    printf("\n");
+    }
+    
 }
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
-printf("Sorted array is : ");
-for (i = 0; i < n; i++)
-{
-printf(" %d ",arr[i]);
-}
-return 0;
+
+int main() {
+    int size, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array: ");
+    printArray(arr, size);
+
+    selectionSort(arr, size);
+
+    printf("Sorted array: ");
+    printArray(arr, size);
+
+    return 0;
 }
